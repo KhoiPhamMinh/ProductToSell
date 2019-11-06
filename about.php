e8786454b04550bbd4decc3e2dc990eee905ca6c
KhoiPhamMinh <khoipmgch18653@fpt.edu.vn>
2019-11-06 16:05:00 +0700

    Update about.php

--- about.php
+++ about.php
@@ -12,9 +12,9 @@
 	<div class="about-section">
 
 <p>ABOUT US:
-ATN is New Zealand's largest footwear retailer with 51 full retail stores throughout the country. Shoes-Shi offer a huge selection of footwear for the whole family - men's, women's and kids' shoes. Whether it's a pair of ankle boots to wear out at night, some comfy slippers to snuggle up in, school shoes for the kids or a pair of running shoes to hit the streets, we have something for everyone.</p>
+ATN is New Zealand's largest Toy's store retailer with 51 full retail stores throughout the country. ATN offer a huge selection of footwear for the whole family - men's, women's and kids' toys. Whether it's a pair of ankle boots to wear out at night, some comfy slippers to snuggle up in, school shoes for the kids or a pair of running shoes to hit the streets, we have something for everyone.</p>
 
-<p>We scour the globe for the hottest trends, bringing back the must-have styles of the season. To complement our fashion-forward offering we also have a huge range of practical footwear at the best value. We aim to keep our prices affordable so you get the latest look without the designer price tag.</p>
+<p>We scour the globe for the hottest trends, bringing back the must-have styles of the season. To complement our fashion-forward offering we also have a huge range of practical Toy at the best value. We aim to keep our prices affordable so you get the latest look without the designer price tag.</p>
 
 
 </div>
